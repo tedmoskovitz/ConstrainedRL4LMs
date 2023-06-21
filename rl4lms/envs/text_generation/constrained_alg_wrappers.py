10b61a6ed2532b3be24f7d59235fc2f9e7adb03f
Ted <thm2118@columbia.edu>
2023-06-21 13:59:28 +0100

    added constraint rewards and constraint reward buffers to alg wrapper; added dummy constraint values

--- rl4lms/envs/text_generation/constrained_alg_wrappers.py
+++ rl4lms/envs/text_generation/constrained_alg_wrappers.py
@@ -425,6 +425,7 @@ def wrap_constrained_alg(
                 rollout_info = self.generate_batch(
                     rollout_buffer, tokenizer, max_steps, rollout_info
                 )
+            pdb.set_trace()
 
             # aggregate rollout info
             aggregated_rollout_info = {}
